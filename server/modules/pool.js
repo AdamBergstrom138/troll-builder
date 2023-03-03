06ba9ffbe257fe73440453ea8ba94db43d4d7495
Adam Bergstrom <abx1138@gmail.com>
2023-03-03 13:15:30 -0600

    updated pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -5,50 +5,52 @@
  change `prime_app` to the name of your database, and you should be all set!
 */
 
-// const pg = require('pg');
-// let pool;
+const pg = require('pg');
+let pool;
 
-// // When our app is deployed to the internet 
-// // we'll use the DATABASE_URL environment variable
-// // to set the connection info: web address, username/password, db name
-// // eg: 
-// //  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
-// if (process.env.DATABASE_URL) {
-//     pool = new pg.Pool({
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: {
-//             rejectUnauthorized: false
-//         }
-//     });
-// }
-// // When we're running this app on our own computer
-// // we'll connect to the postgres database that is 
-// // also running on our computer (localhost)
-// else {
-//     pool = new pg.Pool({
-//         host: 'localhost',
-//         port: 5432,
-//         database: 'solo',   // 	💥 Change this to the name of your database!
-//     });
-// }
+// When our app is deployed to the internet 
+// we'll use the DATABASE_URL environment variable
+// to set the connection info: web address, username/password, db name
+// eg: 
+//  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+// When we're running this app on our own computer
+// we'll connect to the postgres database that is 
+// also running on our computer (localhost)
+else {
+    pool = new pg.Pool({
+        host: 'localhost',
+        port: 5432,
+        database: 'solo',   // 	💥 Change this to the name of your database!
+    });
+}
 
-const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// heroku code failed
+// const { Client } = require('pg');
 
-client.connect();
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 module.exports = pool;
